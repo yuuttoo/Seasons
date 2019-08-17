2991d7c1079e9ed5c955b6730985f3a8cc5b9e85
yuuto <yuuttoo@gmail.com>
2019-08-17 20:47:51 +0800

    Updates

---
+++ precache-manifest.bed1d60df1cd1d06a70dfe8823b05a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d7f4ed95336267f31fc61dc2d7de94a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c76434e217bfc5888a36",
+    "url": "/static/css/main.1b1fc6ce.chunk.css"
+  },
+  {
+    "revision": "7eb4b751cd97c70af689",
+    "url": "/static/js/2.ab09449c.chunk.js"
+  },
+  {
+    "revision": "c76434e217bfc5888a36",
+    "url": "/static/js/main.dcaec9a7.chunk.js"
+  },
+  {
+    "revision": "3527a5ff649e49a4c23d",
+    "url": "/static/js/runtime~main.462137d7.js"
+  }
+]);
